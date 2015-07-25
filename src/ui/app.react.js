80e63cb76cbf6bd9c6ef919bb9cd63df3ce96c70
JP Richardson <jprichardson@gmail.com>
2015-07-25 11:04:49 -0500

    trinity: made window resizable

--- src/ui/app.react.js
+++ src/ui/app.react.js
@@ -2,16 +2,15 @@
 
 // this is the antithesis of how a React application should be made
 
-import React, { PropTypes } from 'react'
+import React from 'react'
 import * as events from '../events'
 import TestView from './test.react'
-window.events= events
 
 const App = React.createClass({
   displayName: 'AppView',
 
   getInitialState () {
-    return { tests: [], results: {} }
+    return { tests: [], results: {}, dragData: {} }
   },
 
   handleTestEvents ({ eventName, data }) {
@@ -34,7 +33,7 @@ const App = React.createClass({
         this.setState({ tests: tests })
         break
       case 'result':
-        results = this.state.results
+        let results = this.state.results
         results.push(results)
         this.setState({ results: results })
         break
@@ -43,25 +42,47 @@ const App = React.createClass({
 
   componentDidMount () {
     events.subscribe(this.handleTestEvents)
+
+    var self = this
+    document.addEventListener('mousemove', function ({ pageX }) {
+      let dragData = self.state.dragData
+      if (!dragData.dragging) return
+      let rootEl = document.getElementById('trinity-root')
+      let offset = pageX - dragData.pageX
+      let newWidth = dragData.width - offset
+      rootEl.style.width = newWidth + 'px'
+    })
+
+    document.addEventListener('mouseup', function ({ pageX }) {
+      self.setState({ dragData: {
+        dragging: false
+      }})
+    })
   },
 
   componentDidUnmount () {
     events.unsubscribe(this.handleTestEvents)
   },
 
-  componentWillUpdate () {
-
+  dragStart ({ pageX }) {
+    let rootEl = document.getElementById('trinity-root')
+    this.setState({ dragData: {
+      dragging: true,
+      pageX: pageX,
+      width: rootEl.offsetWidth
+    }})
   },
 
   render () {
     let tests = this.state.tests.map(test => <TestView test={ test } />)
 
     return (
-      <div className='inset-panel tree-view-resizer'>
+      <div className='inset-panel tree-view-resizer' style={{ width: '100%' }}>
         <div className='panel-heading'>Trinity</div>
         <div className='panel-body padded tree-view-scroller'>
           { tests }
         </div>
+        <div className='tree-view-resize-handle' onMouseDown={ this.dragStart }/>
       </div>
     )
   }
