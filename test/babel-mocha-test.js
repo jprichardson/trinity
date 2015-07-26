8cd2a29692942c8059487dfb8368e4993a1570f4
JP Richardson <jprichardson@gmail.com>
2015-07-26 10:07:50 -0500

    trinity: added babel mocha support

---
+++ test/babel-mocha-test.js
@@ -0,0 +1,17 @@
+import assert from 'assert'
+import mm from '@mock-module'
+
+/* global describe it */
+// trinity: mocha
+
+describe('some module description', function () {
+  describe('someMethod()', function () {
+    it('should do something', function () {
+      console.log('hello')
+      assert('something', 'gonna assert')
+      console.dir({name: 'jp'})
+      assert.equal(mm(3, 4), 7)
+      console.log('done')
+    })
+  })
+})
