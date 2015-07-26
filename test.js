42c4156ee536e715fb02f39515935fd5643f66a0
JP Richardson <jprichardson@gmail.com>
2015-07-26 06:38:02 -0500

    test: updated with comments

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var test = require('tape')
 
 test('test1: testing something', function (t) {
+  console.log('yo\n dude')
   t.assert(true, 'testing')
   t.end()
 })
@@ -8,6 +9,9 @@ test('test1: testing something', function (t) {
 test('test2: testing something else', function (t) {
   t.ok('it is ok', 'ok it is')
   t.equal(1, 1, '1 === 1')
+  t.comment('hello')
+  console.dir({name: 'jp', last: 'richardson'})
+  console.log("{ name: 'jp', last: 'richardson' }")
   t.equal(1, 2, '1 !== 2')
   t.end()
 })
